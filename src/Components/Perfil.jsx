ce2b5c587d218f82c147d158eabbb78ffbd2995f
eric <eric.santos@esarias.com>
2023-11-07 19:21:51 -0600

    sass and modules

---
+++ src/Components/Perfil.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Perfil = () => {
+  return (
+    <div className='perfil'>
+        <h1>Eric Santos</h1>
+        <p>Programaer geting knowlodege</p>
+    </div>
+  )
+}
---
+++ src/Components/Performance.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Performance = () => {
+  return (
+    <div className='performance'>
+        <h2>Contenido o visualizacion</h2>
+    </div>
+  )
+}
